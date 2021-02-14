42cd0d28f589474627b7a849e91f8027152fb790
Zane Calini <47652557+zanecalini@users.noreply.github.com>
2021-02-13 22:16:19 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 // Script.js
-
+let myStorage = window.localStorage
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  if(myStorage.getItem("a") == null){
+    fetch("https://fakestoreapi.com/products")
+      .then(response => response.json())
+      .then(data =>{myStorage.setItem("a", JSON.stringify(data))} )
+  }
 });
